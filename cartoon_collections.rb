a89328cd82c94a8756057f64f5b27440c7d10d2c
Bryan Haney <i.am.bryan.haney@gmail.com>
2020-03-03 05:38:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,9 @@ def roll_call_dwarves(array)# code an argument here
   array.each_with_index{|dwarf,index| puts "#{index}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map{|call| call.capitalize!
 end
 
 def long_planeteer_calls# code an argument here
