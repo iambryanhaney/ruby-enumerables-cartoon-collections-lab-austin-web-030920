118518c00eb8e32968efc8231394cf7857ae4b99
Bryan Haney <i.am.bryan.haney@gmail.com>
2020-03-03 05:39:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.map{|call| call.capitalize!
+  array.map{|call| call.capitalize!}
+  
 end
 
 def long_planeteer_calls# code an argument here
